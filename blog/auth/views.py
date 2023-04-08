b84f02d71b3373be39b1cf6be8ad05b7d3a60cb7
Vladimir <ecrvmal@yandex.ru>
2023-04-08 13:26:38 -0700

    Lesson5 Debugging WTF

--- blog/auth/views.py
+++ blog/auth/views.py
@@ -3,33 +3,75 @@ from flask_login import logout_user, login_user, login_required, current_user
 from werkzeug.security import check_password_hash
 
 from blog.models import User
+from blog.extensions import db
+from blog.forms.user import UserLoginForm
+from blog.forms.user import UserRegisterForm
 
 auth = Blueprint('auth', __name__, static_folder='../static')
 
 
-@auth.route('/login', methods=('GET','POST'))
+@auth.route('/login', methods=('GET', 'POST'))
 def login():
-    if current_user.is_authenticated:
+    if request.method == 'GET':
+        return render_template('auth/login.html')
+
+    errors = []
+    form = UserLoginForm(request.form)
+
+    if request.method == 'POST' and form.validate_on_submit():
+        username = form.username.data
+        password = form.username.data
+
+        user = User.query.filter_by(username=username).first()
+
+        if not user or not check_password_hash(user.password, password):
+            flash('Check your login details')
+            return redirect(url_for('forms.login'))
+
+        login_user(user)
         return redirect(url_for('user.user_list'))
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/register', methods=('GET', 'POST'))
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('user.profile', pk=current_user.id))
+
+    errors = []
+    form = UserRegisterForm(request.form)  # request.form - If there is a value for key,
+    # the form  will display the value after page reload
+    # this need for debugging
+    if request.method == 'POST' and form.validate_on_submit():
+        if User.query.filter_by(email=form.email.data).count():  # User is model
+            form.email.errors.append("email isn't uniq")
+            return render_template('users/register.html', form=form)
+        if User.query.filter_by(username=form.username.data).count():  # User is model
+            form.username.errors.append("username isn't uniq")
+            return render_template('users/register.html', form=form)
+
+        _user = User(
+            username=form.username.data,
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email=form.email.data,
+            birth_year=form.birth_year.data,
+            password=generate_password_hash(form.password.data),
+        )
+
+        db.session.add(_user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            errors.append("Database Commit Error")
+        else:
+            login_user(_user)
 
     return render_template(
-        'auth/login.html',
+        'users/register.html',
+        form=form,
+        errors=errors
     )
-
-
-@auth.route('/login', methods=('POST',))
-def login_post():
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-    user = User.query.filter_by(username=username).first()
-
-    if not user or not check_password_hash(user.password, password):
-        flash('Check your login details')
-        return redirect(url_for('.login'))
-
-    login_user(user)
-    return redirect(url_for('user.user_list'))
 
 
 @auth.route('/logout')
---
+++ blog/forms/user.py
@@ -0,0 +1,24 @@
+from wtforms import StringField, validators, PasswordField, SubmitField, IntegerField, BooleanField
+from flask_wtf import FlaskForm
+
+
+class UserLoginForm(FlaskForm):
+    username = StringField('Username', [validators.DataRequired(),])
+    password = StringField('Password', [validators.DataRequired(),])
+    submit = SubmitField('Login')
+
+
+class UserRegisterForm(FlaskForm):
+    username = StringField('Username')
+    email = StringField('E-mail', [validators.DataRequired(),
+                                   validators.Email()])
+    birth_year = IntegerField('Birth Year')
+    is_staff = BooleanField('Is Staff')
+    password = PasswordField('Password', [
+        validators.DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     [validators.DataRequired(),
+                                      validators.EqualTo('confirm_password',
+                                                         message='Field must be equal to password')])
+    submit = SubmitField('Register')
+
