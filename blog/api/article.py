8f60863256b07fb91ed89e006b12ea221f531fcf
Vladimir <ecrvmal@yandex.ru>
2023-04-26 06:35:55 -0700

    Lesson10 Initial Commit

--- blog/api/article.py
+++ blog/api/article.py
@@ -1,10 +1,28 @@
+import requests
 from flask_combo_jsonapi import ResourceDetail, ResourceList
 from blog.schemas import ArticleSchema
 from blog.extensions import db
 from blog.models import Article
 
 
+class ArticleListEvent(EventsResource):
+    def event_get_count(self, *args, **kwargs):
+        return{'count': Article.query.count()}
+
+    def event_post_count(self):
+        return{'method': request.method}
+
+    def event_get_api_server(self):
+        return {'count': requests.get('https://ifconfig.io/ip').text}
+
+
+class ArticleDetailEvent(EventsResource):
+    def event_get_count_by_author(self, *args, **kwargs):
+        return{'count': Article.query.filter(Article.author_id == kwargs['id']).count()}
+
+
 class ArticleList(ResourceList):
+    events = ArticleListEvent
     schema = ArticleSchema
     data_layer = {
         "session": db.session,
@@ -13,6 +31,7 @@ class ArticleList(ResourceList):
 
 
 class ArticleDetail(ResourceDetail):
+    events = ArticleDetailEvent
     schema = ArticleSchema
     data_layer = {
         "session": db.session,
