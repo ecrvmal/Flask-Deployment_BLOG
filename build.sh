2181d8f1aad8608a3120a6c54b31c683a8fad55b
Vladimir <ecrvmal@yandex.ru>
2023-05-02 04:44:54 -0700

    Postgree 3

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+pg_ctl -D postgresql://user:CDu8F1LcFO42KbqGbyRVcBKXFYaE9h50@dpg-ch5sff4s3fvuobaiaun0-a/db_lj4s start
+sleep 5
 flask db init
 flask db migrate
 flask create-init-tags
